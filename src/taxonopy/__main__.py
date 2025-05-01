32ea9bef4cd595539f70f2b5c09e6c3de25e0e16
Matthew Thompson <thompson.4509@osu.edu>
2025-05-01 10:21:41 -0400

    - Roll common names apparoch into CLI as subcommand for post-resolution processing - Enable custom cache directory specification - Make outputs recreate input directory hierarchy

--- src/taxonopy/__main__.py
+++ src/taxonopy/__main__.py
@@ -4,7 +4,8 @@ This module provides the entry point for running the package as a script.
 It imports and calls the main function from the cli module.
 """
 
+import sys
 from taxonopy.cli import main
 
 if __name__ == "__main__":
-    exit(main())
+    sys.exit(main())
