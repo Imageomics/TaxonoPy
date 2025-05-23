b84ff0d9f6429c2422ce7cdf3c9e81807ccdeb57
Matthew Thompson <thompson.4509@osu.edu>
2025-05-23 15:03:37 -0400

    Change version format to comply with PyPI's requirement for PEP 440 compliance

--- src/taxonopy/__init__.py
+++ src/taxonopy/__init__.py
@@ -5,7 +5,7 @@ consistent taxonomic hierarchy from a variety of inputs, primarily designed for
 the TreeOfLife (TOL) dataset.
 """
 
-__version__ = "0.1.0-beta"
+__version__ = "0.1.0b0"
 
 from taxonopy.types.data_classes import (
     ResolutionStatus,
